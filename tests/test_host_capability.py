37cfcfd0442ba979d602ae86e898663621dfba28
Eva <eva@100yen.org>
2026-07-21 17:31:49 +0700

    test: include lcm_compute in registration expectations

--- tests/test_host_capability.py
+++ tests/test_host_capability.py
@@ -8,6 +8,7 @@ from pathlib import Path
 EXPECTED_LCM_TOOLS = {
     "lcm_grep",
     "lcm_recall",
+    "lcm_compute",
     "lcm_recent",
     "lcm_load_session",
     "lcm_describe",
--- tests/test_packaging_install.py
+++ tests/test_packaging_install.py
@@ -11,6 +11,7 @@ import types
 EXPECTED_LCM_TOOLS = {
     "lcm_grep",
     "lcm_recall",
+    "lcm_compute",
     "lcm_recent",
     "lcm_load_session",
     "lcm_describe",
